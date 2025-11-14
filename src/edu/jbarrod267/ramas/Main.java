a83b072f0bcdc2fb27722f4b50bdc4a9933dfa54
jbarrod267 <jbarrod267@g.educaand.es>
2025-11-14 14:28:05 +0100

    cambio variable a bolas1

--- src/edu/jbarrod267/ramas/Main.java
+++ src/edu/jbarrod267/ramas/Main.java
@@ -6,15 +6,15 @@ import java.util.Random;
 public class Main {
 	public static void main(String[] args) {
 		// Lotería Primitiva: 5 números principales (1..49) + 1 complementario
-		HashSet<Integer> bolas = new HashSet<>();
+		HashSet<Integer> bolas1 = new HashSet<>();
 		Random r = new Random();
-		while (bolas.size() < 5) {
-			bolas.add(r.nextInt(49) + 1);
+		while (bolas1.size() < 5) {
+			bolas1.add(r.nextInt(49) + 1);
 		}
 		System.out.println("¡Suerte en el sorteo, DAM!");
 		int complementario = r.nextInt(49) + 1;
 		System.out.println("Complementario: " + complementario);
-		int suma = bolas.stream().mapToInt(Integer::intValue).sum();
+		int suma = bolas1.stream().mapToInt(Integer::intValue).sum();
 		System.out.println("Suma de principales: " + suma);
 	}
 }
